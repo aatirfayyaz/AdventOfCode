f398528340379d2432c41d7f73d90586c90b3dd5
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2023-12-08 17:24:31 -0500

    Completed Day1, part1

--- day1/day1.c
+++ day1/day1.c
@@ -11,11 +11,12 @@ int main() {
     
     // Store the content of the file in a buffer
     char buffer[128];
+    int total = 0;
 
     // Read the content of the file, line by line
     while(fgets(buffer, 128, fptr)) {
         
-        printf("Currently reading: %s\n", buffer);
+        // printf("Currently reading: %s\n", buffer);
         int size = strlen(buffer);
         int i = 0;
         char firstNumber, lastNumber;
@@ -24,7 +25,7 @@ int main() {
 
             if(isdigit(buffer[i])) {
                 firstNumber = buffer[i];
-                printf("First number is %c\n", firstNumber);
+                // printf("First number is %c\n", firstNumber);
                 break;
             }
             
@@ -33,19 +34,29 @@ int main() {
 
         i = size;
 
-        while (i > 0) {
+        while (i >= 0) {
 
             if(isdigit(buffer[i])) {
                 lastNumber = buffer[i];
-                printf("Last number is %c\n", lastNumber);
+                // printf("Last number is %c\n", lastNumber);
+                
+                // printf("First and last numbers are %c %c\n", firstNumber, lastNumber);
+                int subtotal;
+                subtotal = (firstNumber - 48) *10 + (lastNumber - 48);
+                // printf("SubTotal = %d\n", subtotal);
+                total += subtotal;
+                // printf("Total = %d\n", total);
+
                 break;
             }
             
             i -= 1;
-        }
-        
+            
+        };
 
-    }
+    };
+
+    printf("Total = %d\n", total);
 
     // Close the file
     fclose(fptr);
