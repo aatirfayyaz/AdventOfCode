91219e38f32e163dab7c32aea4a191dfb28369f9
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2023-12-08 16:49:56 -0500

    Day1 hackey code working

--- day1/day1.c
+++ day1/day1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
 
@@ -7,6 +8,47 @@ int main() {
 
     // Open a file in read mode
     fptr = fopen("data.txt", "r");
+    
+    // Store the content of the file in a buffer
+    char buffer[128];
+
+    // Read the content of the file, line by line
+    while(fgets(buffer, 128, fptr)) {
+        
+        printf("Currently reading: %s\n", buffer);
+        int size = strlen(buffer);
+        int i = 0;
+        char firstNumber, lastNumber;
+
+        while (i < size){
+
+            if(isdigit(buffer[i])) {
+                firstNumber = buffer[i];
+                printf("First number is %c\n", firstNumber);
+                break;
+            }
+            
+            i += 1;
+        }
+
+        i = size;
+
+        while (i > 0) {
+
+            if(isdigit(buffer[i])) {
+                lastNumber = buffer[i];
+                printf("Last number is %c\n", lastNumber);
+                break;
+            }
+            
+            i -= 1;
+        }
+        
+
+    }
+
+    // Close the file
+    fclose(fptr);
 
     return 0;
 }
