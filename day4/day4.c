9b590f9d099e1b1c7f707fdb05f301da505bbe28
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2023-12-18 18:09:10 -0500

    Completed d4p2

--- day4/day4.c
+++ day4/day4.c
@@ -2,22 +2,34 @@
 #include <string.h>
 #include <ctype.h>
 
+
+int inWinningNums(int *winningNums, int n, int val) {
+    for(int i = 0; i < n; i++) {
+        if(winningNums[i] == val)
+            return 1;
+    }
+    return 0;
+};
+
+
+
 int main() {
 
     FILE *fptr;
-    const int rows = 5;
-    const int columns = 48;
+    const int rows = 187;
+    const int columns = 116;
+    const int firstNum = 10;
+    const int lastNum = 39;
 
     // Opening data file in read mode
     fptr = fopen("data.txt", "r");
-    
-    // Storing the content of the file in a buffer
-    char buffer[256];
-    int prevgameID = 0;
-    int total = 0;
 
-    // Array for spelled out numbers
-    char *arr[] = { "red", "blue", "green"};
+    int total = 0;
+    int numCards[187];
+
+    for (int i = 0; i < 187; i++) { 
+        numCards[i] = 1;
+    };
 
     // char buffer[columns];
     char fullFile[rows][columns];
@@ -32,12 +44,70 @@ int main() {
     };
 
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
+        
+        int k = 0;
+        int winningNums[10] = {0};
+        int cardNums[25] = {0};
 
-            printf("%c", fullFile[i][j]);
+        for (int j = firstNum; j <= lastNum; j++) {
+            if (k >= 10) { break; };
+            if (fullFile[i][j] == ' ') {
+                if (fullFile[i][j-1] == ' ') {
+                    continue;
+                }
+                else {
+                    k++;
+                }
+            }
+            else {
+                winningNums[k] = winningNums[k] * 10 + (fullFile[i][j] - '0');
+                // printf("winningNums[%i] = %i\n", k, winningNums[k]);
+            };
+        };
+
+        k = 0;
+
+        for (int j = lastNum + 3; j < columns; j++) {
+            if (k >= 25) { break; };
+            if (fullFile[i][j] == ' ') {
+                if (fullFile[i][j-1] == ' ') {
+                    continue;
+                }
+                else {
+                    k++;
+                }
+            }
+            else {
+                cardNums[k] = cardNums[k] * 10 + (fullFile[i][j] - '0');
+                // printf("cardNums[%i] = %i\n", k, cardNums[k]);
+            };
+        };
+
+        int match = 0;
+
+        for (int j = 0; j < 25; j++) {
+
+            if (inWinningNums(winningNums, 10, cardNums[j])) {
+                // printf("Found %i in winningNums in row[%i]\n", cardNums[j], i);
+                match++;
+            };
 
         };
+
+        for (int j = 0; j < match; j++) {
+
+            numCards[i+j+1] += numCards[i];
+            // printf("numCards[%i] = %i\n", i + j, numCards[i+j]);
+
+        };
+        
+        total += numCards[i];
+        printf("Match value at row[%i] = %i, and total now = %i\n", i, match, total);
+    
     };
 
+    printf("The total is: %i", total);
 
+    fclose(fptr);
+    return 0;
 };
