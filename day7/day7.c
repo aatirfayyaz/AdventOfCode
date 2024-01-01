eaa665f90a2ba44fd58b7208d4b3e064f6ab142c
Aatir Fayyaz <aatir.fayyaz@columbia.edu>
2024-01-01 15:17:01 -0500

    working on d7

--- day7/day7.c
+++ day7/day7.c
@@ -10,10 +10,10 @@
 
 #define __STDC_FORMAT_MACROS
 #define MAX_LINE_NUMBER 300
-#define MAX_LINE_SIZE 300
+#define MAX_LINE_SIZE 15
 
 enum handTypes {
-    fiveOfAKind,
+    fiveOfAKind = 0,
     fourOfAKind,
     fullHouse,
     threeOfAKind,
@@ -29,12 +29,74 @@ struct game {
     int rank;
 };
 
-enum handTypes getHandType(char *hand[5]) {
-    char seen[13] = {0};
+void getHandTypes(struct game *game) {
+    char unique[5]; //Change based on line nb
+    int counted = 0;
+    int maxCount = 0;
+
     for (int i = 0; i < 5; i++) {
-        
+        bool already_counted = false;
+        for (int j = 0; j < counted; j++) {
+            if (game->hand[i] == unique[j]) already_counted = true;
+        }
+
+        if (already_counted) continue;
+
+        int count = 0;
+        for (int j = 0; j < 5; j++) {
+            if (game->hand[i] == game->hand[j]) { 
+                count++;
+                if (count > maxCount) maxCount = count;
+            } 
+        }
+
+        unique[counted] = game->hand[i];
+        // printf("Char: %c has count = %i and saved at unique[%i] = %c & maxCount = %i\n", 
+        //         game.hand[i], count, counted, game.hand[i], maxCount);
+        counted++;
+
     }
+
+    if (counted == 1) {
+        // printf("fiveOfAKind: %c\n", unique[0]);
+        game->handType = fiveOfAKind;
+    }
+    if (counted == 2) {
+        if (maxCount == 3) {
+            // printf("fullHouse: %c & %c\n", unique[0], unique[1]);
+            game->handType = fullHouse;
+        }
+        else { 
+            game->handType = fourOfAKind;
+            // printf("fourOfAKind: %c & %c\n", unique[0], unique[1]);
+        }
+    }
+    if (counted == 3) {
+        if (maxCount == 3) {
+            game->handType = threeOfAKind;
+            // printf("threeOfAKind: %c, %c & %c\n", unique[0], unique[1], unique[2]);
+        }
+        else {
+            game->handType = twoPair;
+            // printf("twoPair: %c, %c & %c\n", unique[0], unique[1], unique[2]);
+
+        }
+    }
+    if (counted == 4) {
+        game->handType = onePair;
+        // printf("onePair: %c, %c, %c & %c\n", unique[0], unique[1], unique[2], unique[3]);
+    }
+    if (counted == 5) {
+        game->handType = highCard;
+        // printf("highCard: %c, %c, %c, %c & %c\n", unique[0], unique[1], unique[2], unique[3], unique[4]);
+    }
+
 };
+
+
+void rankHands(struct game *game) {
+
+}
 
 void bad_input()
 {
@@ -68,12 +130,16 @@ int main(int argc, char**argv)
     for (int i = 0; i < line_nb; i++) {
         char *ptr = input[i];
         strncpy(games[i].hand, ptr, 5);
+        getHandTypes(&games[i]);
         games[i].rank = 0;
-        games[i].handType = getHandType(games[i].hand);
         games[i].bid = strtol(ptr + 6, &ptr, 10);
 
         printf("hand = %s, bid = %i, handType = %i, rank = %i\n", games[i].hand, games[i].bid, games[i].handType, games[i].rank);
     };
+
+    for (int i = 0; i < line_nb; i++) {
+        
+    }
 
     return(0);
 
